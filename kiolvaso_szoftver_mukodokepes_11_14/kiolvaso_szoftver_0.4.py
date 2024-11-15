16c6853ca9950efae29ca25c7c40188d9b3016ce
Pisti22 <istvan.olah0423@gmail.com>
2024-11-15 14:27:38 +0100

    0.5 version UPDATE

--- kiolvaso_szoftver_mukodokepes_11_14/kiolvaso_szoftver_0.4.py
+++ kiolvaso_szoftver_mukodokepes_11_14/kiolvaso_szoftver_0.4.py
@@ -31,16 +31,10 @@ else:
     logger.info("Sikerült csatlakozni!")
 
 
-f = input("Hany fazison szeretnel merni?")
-
-if f == 1:
-    reg = [3027,2999,3075,3059,3067,3109,3195,3239,3207,3223] #1phase
-
-else:
-    reg = [3027,3029,3031,2999,3001,30003,3075,3059,3067,3109,3195,3239,3207,3223] #3phase
+reg = [3027,3029,3031,2999,3001,30003,3075,3059,3067,3109,3195,3239,3207,3223] 
 
 
-Regiszterek = [3028,3030,3032,   3000,3002,3004,       3076,3060,3068,   3110,      3196,               3240,3208,3224         ]
+Regiszterek = [3028,3030,3032,3000,3002,3004,3076,3060,3068,3110,3196,3240,3208,3224]
                 #u1,u2,u3 fesz   i1,i2,i3 áramerősség                   frekvencia,  IEc,           kumulált látsz,hat,meddő energiamenny
                 #(3070,3072,3074)látszólagos telj,       (3054,3056,3058, )hatásos teljesitmeny    (3062,3064,3066,)meddő teljesitmeny
 
@@ -48,8 +42,9 @@ Regiszterek = [3028,3030,3032,   3000,3002,3004,       3076,3060,3068,   3110,  
 
 def olvas():
 
-    olvasott = [] #üres lista
+    olvasott = [] 
     n=0
+    
     for beolvasott in reg:
 
         if beolvasott == 3239 or beolvasott == 3207 or beolvasott == 3223:
@@ -67,12 +62,22 @@ def olvas():
             decoder=BinaryPayloadDecoder.fromRegisters(x.registers,byteorder=Endian.BIG, wordorder=Endian.BIG)
             y=decoder.decode_32bit_float() 
 
+        
 
-        olvasott.append(y)
-        logger.info(str(Regiszterek[n])+'A register olvasasa sikeres, Erteke:'+str(y))
-        n=n+1
-        continue
+        if y == 'non':
+            y=0
+            olvasott.append(y)
+            logger.info(str(Regiszterek[n])+'A register olvasasa sikeres, Erteke:'+str(y))
+            n=n+1
+            continue
+        
+        else:
+            olvasott.append(y)
+            logger.info(str(Regiszterek[n])+'A register olvasasa sikeres, Erteke:'+str(y))
+            n=n+1
+            continue
 
+    
     return olvasott
 
 
@@ -91,7 +96,7 @@ def iras(adat):
 
     
         
-    for n in range(len(reg)):
+    for n in range(14):
 
         datum = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
